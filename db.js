236cc8c1fa1911fbb9a96d961884020d8574d851
Bavani1125 <bavanikarthikeyan11@gmail.com>
2025-12-09 03:00:29 +0530

    Remove .env for Railway

--- db.js
+++ db.js
@@ -2,21 +2,21 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-// Create PostgreSQL pool (shared across controllers)
+const connectionString = process.env.DATABASE_URL;
+
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mythforge",
-  password: "Sunil@1996",
-  port: 5432,
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// Optional: Test DB connection once when server starts
+// Test connection
 (async () => {
   try {
     const client = await pool.connect();
     const res = await client.query("SELECT NOW()");
-    console.log("✅ Connected to PostgreSQL successfully:", res.rows[0].now);
+    console.log("✅ Connected to PostgreSQL:", res.rows[0].now);
     client.release();
   } catch (err) {
     console.error("❌ Database connection error:", err.message);
